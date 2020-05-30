2a14748f826eda37452ed3c83c8a28f178b70bfe
Sven Dildick <sven.dildick@cern.ch>
2020-05-30 13:25:46 -0500

    Update struct

--- GEMValidation/interface/Structs/CSCSimHitStruct.h
+++ GEMValidation/interface/Structs/CSCSimHitStruct.h
@@ -10,64 +10,54 @@ namespace gem {
     static const int nStations = 11;
 
     // bools
-    bool chamber_ME1_csc_sh_odd[nStations];
-    bool chamber_ME1_csc_sh_even[nStations];
-
-    bool chamber_ME2_csc_sh_even[nStations];
-    bool chamber_ME2_csc_sh_odd[nStations];
-
     bool has_csc_sh_odd[nStations];
     bool has_csc_sh_even[nStations];
 
     // ints
-    Int_t nlayers_csc_sh_even[nStations];
-    Int_t nlayers_csc_sh_odd[nStations];
+    int nlayers_csc_sh_even[nStations];
+    int nlayers_csc_sh_odd[nStations];
 
     int chamber_sh_odd[nStations];
     int chamber_sh_even[nStations];
 
     // floats
-    Float_t dphi_sh_even[nStations];
-    Float_t dphi_sh_odd[nStations];
+    float dphi_sh_even[nStations];
+    float dphi_sh_odd[nStations];
 
-    Float_t dphipositionpt_cscsh_even[nStations];
+    float dphipositionpt_cscsh_even[nStations];
     float dphipositionpt_cscsh_odd[nStations];
 
-    Float_t bending_sh[nStations];
-    Float_t phi_cscsh_even[nStations];
+    float bending_sh[nStations];
+    float phi_cscsh_even[nStations];
     float phi_cscsh_odd[nStations];
     float eta_cscsh_even[nStations];
     float eta_cscsh_odd[nStations];
+    float perp_cscsh_even[nStations];
+    float perp_cscsh_odd[nStations];
 
-    Float_t phi_layer1_sh_even[nStations];
+    float phi_layer1_sh_even[nStations];
     float eta_layer1_sh_even[nStations];
     float phi_layer1_sh_odd[nStations];
     float eta_layer1_sh_odd[nStations];
     float perp_layer1_sh_odd[nStations];
     float perp_layer1_sh_even[nStations];
-    Float_t z_layer1_sh_odd[nStations];
+    float z_layer1_sh_odd[nStations];
     float z_layer1_sh_even[nStations];
-    Float_t phi_layer6_sh_even[nStations];
+
+    float phi_layer6_sh_even[nStations];
     float eta_layer6_sh_even[nStations];
     float phi_layer6_sh_odd[nStations];
     float eta_layer6_sh_odd[nStations];
     float perp_layer6_sh_odd[nStations];
     float perp_layer6_sh_even[nStations];
-    Float_t z_layer6_sh_odd[nStations];
+    float z_layer6_sh_odd[nStations];
     float z_layer6_sh_even[nStations];
-    float perp_cscsh_even[nStations];
-    float perp_cscsh_odd[nStations];
 
     void init() {
       for (unsigned i = 0 ; i < nStations; i++) {
 
-        bending_sh[i] = -10;
-
-        chamber_ME1_csc_sh_even[i] = 0;
-        chamber_ME1_csc_sh_odd[i] = 0;
-
-        chamber_ME2_csc_sh_even[i] = 0;
-        chamber_ME2_csc_sh_odd[i] = 0;
+        has_csc_sh_even[i] = false;
+        has_csc_sh_odd[i] = false;
 
         chamber_sh_odd[i] = -1;
         chamber_sh_even[i] = -1;
@@ -84,44 +74,37 @@ namespace gem {
         eta_cscsh_even[i] = -9.0;
         eta_cscsh_odd[i] = -9.0;
 
-        has_csc_sh_even[i] = false;
-        has_csc_sh_odd[i] = false;
-
         dphi_sh_odd[i] = -9;
         dphi_sh_even[i] = -9;
+
+        bending_sh[i] = -10;
       }
     };
 
     void book(TTree* t) {
 
-      t->Branch("bending_sh", &bending_sh);
+      t->Branch("has_csc_sh_even", has_csc_sh_even, "has_csc_sh_even[nStations]/O");
+      t->Branch("has_csc_sh_odd", has_csc_sh_odd, "has_csc_sh_odd[nStations]/O");
 
-      t->Branch("has_csc_sh_even", &has_csc_sh_even);
-      t->Branch("has_csc_sh_odd", &has_csc_sh_odd);
+      t->Branch("chamber_sh_odd", chamber_sh_odd, "chamber_sh_odd[nStations]/I");
+      t->Branch("chamber_sh_even", chamber_sh_even, "chamber_sh_even[nStations]/I");
 
-      t->Branch("chamber_ME1_csc_sh_odd", &chamber_ME1_csc_sh_odd);
-      t->Branch("chamber_ME1_csc_sh_even", &chamber_ME1_csc_sh_even);
+      t->Branch("nlayers_csc_sh_odd", nlayers_csc_sh_odd, "nlayers_csc_sh_odd[nStations]/I");
+      t->Branch("nlayers_csc_sh_even", nlayers_csc_sh_even, "nlayers_csc_sh_even[nStations]/I");
 
-      t->Branch("chamber_ME2_csc_sh_odd", &chamber_ME2_csc_sh_odd);
-      t->Branch("chamber_ME2_csc_sh_even", &chamber_ME2_csc_sh_even);
+      t->Branch("bending_sh", bending_sh, "bending_sh[nStations]/I");
 
-      t->Branch("chamber_sh_odd", &chamber_sh_odd);
-      t->Branch("chamber_sh_even", &chamber_sh_even);
+      t->Branch("perp_cscsh_odd", perp_cscsh_odd, "perp_cscsh_odd[nStations]/F");
+      t->Branch("perp_cscsh_even", perp_cscsh_even, "perp_cscsh_even[nStations]/F");
 
-      t->Branch("nlayers_csc_sh_odd", &nlayers_csc_sh_odd);
-      t->Branch("nlayers_csc_sh_even", &nlayers_csc_sh_even);
+      t->Branch("phi_cscsh_even", phi_cscsh_even, "phi_cscsh_even[nStations]/F");
+      t->Branch("phi_cscsh_odd", phi_cscsh_odd, "phi_cscsh_odd[nStations]/F");
 
-      t->Branch("perp_cscsh_odd", &perp_cscsh_odd);
-      t->Branch("perp_cscsh_even", &perp_cscsh_even);
+      t->Branch("eta_cscsh_even", eta_cscsh_even, "eta_cscsh_even[nStations]/F");
+      t->Branch("eta_cscsh_odd", eta_cscsh_odd, "eta_cscsh_odd[nStations]/F");
 
-      t->Branch("phi_cscsh_even", &phi_cscsh_even);
-      t->Branch("phi_cscsh_odd", &phi_cscsh_odd);
-
-      t->Branch("eta_cscsh_even", &eta_cscsh_even);
-      t->Branch("eta_cscsh_odd", &eta_cscsh_odd);
-
-      t->Branch("dphi_sh_odd", &dphi_sh_odd);
-      t->Branch("dphi_sh_even", &dphi_sh_even);
+      t->Branch("dphi_sh_odd", dphi_sh_odd, "dphi_sh_odd[nStations]/F");
+      t->Branch("dphi_sh_even", dphi_sh_even, "dphi_sh_even[nStations]/F");
     }
   };
 }  // namespace
