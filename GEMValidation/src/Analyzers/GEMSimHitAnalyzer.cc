2f0b32a57646f53d72fb37711b253d9152f16971
Sven Dildick <sven.dildick@cern.ch>
2020-05-30 16:16:21 -0500

    fix gem simhit matcher

--- GEMValidation/src/Analyzers/GEMSimHitAnalyzer.cc
+++ GEMValidation/src/Analyzers/GEMSimHitAnalyzer.cc
@@ -18,37 +18,27 @@ void GEMSimHitAnalyzer::analyze(TreeManager& tree)
 
     const bool odd(id.chamber()%2==1);
 
-    auto& match = match_;
-
-    auto& gemSimHit = tree.gemSimHit();
-
-    auto fill = [gemSimHit, d, match, odd] (int st) mutable {
-
-      if (match->hitsInSuperChamber(d).size() > 0) {
-        if (odd) {
-          gemSimHit.has_gem_sh_odd[st] = true;
-        }
-        else {
-          gemSimHit.has_gem_sh_even[st] = true;
-        }
-        const float mean_strip(match->simHitsMeanStrip(match->hitsInSuperChamber(d)));
-        if (odd) gemSimHit.strip_gemsh_odd[st] = mean_strip;
-        else     gemSimHit.strip_gemsh_even[st] = mean_strip;
+    if (match_->hitsInSuperChamber(d).size() > 0) {
+      if (odd) {
+        tree.gemSimHit().has_gem_sh_odd[st] = true;
       }
-      if (match->nLayersWithHitsInSuperChamber(d) >= 2) {
-        if (odd) gemSimHit.has_gem_sh2_odd[st] = true;
-        else     gemSimHit.has_gem_sh2_even[st] = true;
+      else {
+        tree.gemSimHit().has_gem_sh_even[st] = true;
       }
-      const auto& copad_superids (match->superChamberIdsCoincidences());
-      if (copad_superids.find(d) != copad_superids.end()) {
-        if (odd) gemSimHit.has_gemcopad_sh_odd[st] = true;
-        else     gemSimHit.has_gemcopad_sh_even[st] = true;
-      }
-    };
-
-    fill(st);
-    if (st==2 or st==3) {
-      fill(1);
+      const float mean_strip(match_->simHitsMeanStrip(match_->hitsInSuperChamber(d)));
+      if (odd) tree.gemSimHit().strip_gemsh_odd[st] = mean_strip;
+      else     tree.gemSimHit().strip_gemsh_even[st] = mean_strip;
     }
-  }
+
+    if (match_->nLayersWithHitsInSuperChamber(d) >= 2) {
+      if (odd) tree.gemSimHit().has_gem_sh2_odd[st] = true;
+      else     tree.gemSimHit().has_gem_sh2_even[st] = true;
+    }
+
+    const auto& copad_superids (match_->superChamberIdsCoincidences());
+    if (copad_superids.find(d) != copad_superids.end()) {
+      if (odd) tree.gemSimHit().has_gemcopad_sh_odd[st] = true;
+      else     tree.gemSimHit().has_gemcopad_sh_even[st] = true;
+    }
+  };
 }
