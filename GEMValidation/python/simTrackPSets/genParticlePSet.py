cb1c60f7f5b8e41c94362202616409e6b583920b
Sven Dildick <sven.dildick@cern.ch>
2021-02-09 13:37:07 -0600

    add LLP PDG Ids

--- GEMValidation/python/simTrackPSets/genParticlePSet.py
+++ GEMValidation/python/simTrackPSets/genParticlePSet.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 genParticlePSet = cms.PSet(
     genParticle = cms.PSet(
         pdgIds = cms.vint32(13,-13),
+        #for LLP efficiency studies
+        #pdgIds = cms.vint32(9000006, -9000006, 9000007, -9000007),
         stableParticle = cms.bool(False),
         etaMin = cms.double(-2.4),
         etaMax = cms.double(2.4),
