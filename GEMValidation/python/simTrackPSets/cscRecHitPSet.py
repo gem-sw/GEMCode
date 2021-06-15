d9a1879a452d41659669813b2eaa54b7cf94e98b
Sven Dildick <sven.dildick@cern.ch>
2021-06-15 14:40:33 -0500

    add missing pset

--- GEMValidation/python/simTrackPSets/cscRecHitPSet.py
+++ GEMValidation/python/simTrackPSets/cscRecHitPSet.py
@@ -1,6 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 cscRecHitPSet = cms.PSet(
+    #csc CLCT, central BX 7
+    cscPreCLCT = cms.PSet(
+        verbose = cms.int32(0),
+        inputTag = cms.InputTag("simCscTriggerPrimitiveDigis"),
+        minBX = cms.int32(6),
+        maxBX = cms.int32(8),
+        minNHitsChamber = cms.int32(3),
+    ),
     cscRecHit = cms.PSet(
         verbose = cms.int32(0),
         inputTag = cms.InputTag("csc2DRecHits"),
